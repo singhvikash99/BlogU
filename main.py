2dd4d0b080079769124c2efc3b2b7130ee153297
Vikash <jhonslax.bs@gmail.com>
2024-02-26 15:24:32 +0530

    connected & stored signup data to database

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import models, db
+from sqlalchemy import select
+from datetime import datetime
 
 app=Flask(__name__)
 
@@ -11,8 +14,37 @@ def user_login():
     pass
     return render_template("index.html")
 
-def user_signup():
-    return render_template("index.html")
+@app.route('/user/signup/', methods=["POST"])
+def user_signup(db = db.db_session()): #db is filename, db_session is defined funtion to start a session
+    try:
+        form_data = dict(request.form)
+
+        db_user = select(models.Users).where(models.Users.Email == form_data["email"])
+        if db_user != []:
+            db.close()
+            return render_template ("index.html", err_msg = f"User with {form_data['email']} already exists")
+
+        new_user = models.Users()
+        new_user.First_name = form_data["f_name"]
+        new_user.Last_name = form_data["l_name"]
+        new_user.Email = form_data["email"]
+        new_user.Ph_no = form_data["contact"]
+        new_user.Pass = form_data["password"]
+        new_user.Created_at = datetime.now()
+        new_user.Updated_at = datetime.now()
+        new_user.Username = form_data["username"]
+        db.add(new_user)
+        db.commit()
+        db.close()
+
+        return redirect('/')
+    
+    except Exception as err:
+        db.close()
+        return {"details": str(err)}
+
+    
+    
 
 
 
